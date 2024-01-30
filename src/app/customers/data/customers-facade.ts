aa2317977f333e23bea21996e271a7e85943cf73
David-ben-Mesecke <chr.schiffer@gmail.com>
2024-01-30 02:45:49 +0100

    Refactor customers-facade.ts imports and class properties to my own code standards

--- src/app/customers/data/customers-facade.ts
+++ src/app/customers/data/customers-facade.ts
@@ -1,10 +1,11 @@
-import { inject, Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+
+import { Customer } from '@app/customers/model';
+import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { customersActions } from './customers.actions';
-import { Observable } from 'rxjs';
-import { Customer } from '@app/customers/model';
-import { fromCustomers } from './customers.selectors';
 import { filterDefined } from '@app/shared';
+import { fromCustomers } from './customers.selectors';
 import { map } from 'rxjs/operators';
 
 function deepClone<T>(source$: Observable<T>): Observable<T> {
@@ -13,38 +14,38 @@ function deepClone<T>(source$: Observable<T>): Observable<T> {
 
 @Injectable({ providedIn: 'root' })
 export class CustomersFacade {
-  #isLoaded = false;
-  #store = inject(Store);
+  isLoaded = false;
+  _store = inject(Store);
 
   get customers$(): Observable<Customer[]> {
     this.#assertLoaded();
-    return this.#store.select(fromCustomers.selectAll);
+    return this._store.select(fromCustomers.selectAll);
   }
 
   byId(id: number): Observable<Customer> {
     this.#assertLoaded();
 
-    return this.#store
+    return this._store
       .select(fromCustomers.selectById(id))
       .pipe(filterDefined, deepClone);
   }
 
   #assertLoaded() {
-    if (!this.#isLoaded) {
-      this.#store.dispatch(customersActions.load());
-      this.#isLoaded = true;
+    if (!this.isLoaded) {
+      this._store.dispatch(customersActions.load());
+      this.isLoaded = true;
     }
   }
 
   remove(id: number) {
-    this.#store.dispatch(customersActions.remove({ id }));
+    this._store.dispatch(customersActions.remove({ id }));
   }
 
   update(customer: Customer) {
-    this.#store.dispatch(customersActions.update({ customer }));
+    this._store.dispatch(customersActions.update({ customer }));
   }
 
   add(customer: Customer) {
-    this.#store.dispatch(customersActions.add({ customer }));
+    this._store.dispatch(customersActions.add({ customer }));
   }
 }
